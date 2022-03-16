0d6017c718353b5ae4490623851986ef6220eb94
Marcos Fernando <estudogit@outlook.com>
2022-03-16 15:49:57 -0300

    classe jogador terminada

--- jogo_python_udemy/jogador.py
+++ jogo_python_udemy/jogador.py
@@ -1,13 +1,13 @@
 class Jogador:
 
-    def __init__(self, pontuacao, vida):
-        self.__pontuacao = pontuacao
+    def __init__(self, vida: int, pontuacao: int = 0):
         self.__vida = vida
+        self.__pontuacao = pontuacao
+        
+    @property
+    def vida(self):
+        return self.__vida
 
     @property
     def pontuacao(self):
         return self.__pontuacao
-
-    @property
-    def vida(self):
-        return self.__vida
