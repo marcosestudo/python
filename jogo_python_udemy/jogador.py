f8c6aecfb810ae35a624f2a51db6afc1ac521e1e
Marcos Fernando <estudogit@outlook.com>
2022-03-16 16:19:18 -0300

    funcionando perfeitamente, mas precisa refatorar

--- jogo_python_udemy/jogador.py
+++ jogo_python_udemy/jogador.py
@@ -8,6 +8,14 @@ class Jogador:
     def vida(self):
         return self.__vida
 
+    @vida.setter
+    def vida(self, vida):
+        self.__vida = vida    
+
     @property
     def pontuacao(self):
         return self.__pontuacao
+
+    @pontuacao.setter
+    def pontuacao(self, pontuacao):
+        self.__pontuacao = pontuacao    
--- jogo_python_udemy/jogo.py
+++ jogo_python_udemy/jogo.py
@@ -1,10 +1,12 @@
 import random
+from jogador import Jogador
 
 
-pontuacao = 0
+dificuldade = None
 
 
-dificuldade = input('''Escolha a dificuldade: digite 1, 2 ou 3
+while dificuldade not in ('1', '2', '3'):
+    dificuldade = input('''Escolha a dificuldade: digite 1, 2 ou 3
                        1 para fácil
                        2 para média
                        3 para difícil
@@ -12,30 +14,93 @@ dificuldade = input('''Escolha a dificuldade: digite 1, 2 ou 3
 
 
 if dificuldade == '1':
-    vida, vida_perdida = 20, 0
+    j = Jogador(20) 
+    vida_perdida = 0
     
     while True:
         # cehcagem testa se o input é um um número entre 0 e 1_000_000
         checagem = (str(x) for x in range(1000000))
         if random.choice([1, 2, 3]) == 1 or 2 or 3:
             a, b = random.randint(0, 9), random.randint(0, 9)
-            resposta = input(f'{a} + {b} = ')
+            resposta = input(f'\n{a} + {b} = ')
             
             while resposta not in checagem:
-                checagem = (str(x) for x in range(1000000))
+                # checagem = (str(x) for x in range(1000000))
                 resposta = input('Insira um número inteiro')
-            
 
             if int(resposta) == a + b:
-                print('\ncorreto\npontuação++\n')
-                pontuacao += 1
+                print('\ncorreto\n')
+                j.pontuacao += 1
             else:
-                print('\nerrado\nvida--\n')
+                print('\nerrado\n')
                 vida_perdida += 4
-                vida -= 4
+                j.vida -= 4
 
-        print(f"Vida ]{'|' * (vida)}{int(vida / 20 * 100)}%{' ' * vida_perdida}[")
-        print(f'Pontuacao {pontuacao}\n')
+        print(f"Vida: ]{'|' * (j.vida)}{int(j.vida / 20 * 100)}%{'-' * vida_perdida}[")
+        print(f'Pontuacao: {j.pontuacao}\n')
 
-        if vida == 0:
+        if j.vida == 0 or input('Continuar? s / n\n' ) == 'n':
+            print(f'\nSua pontuação é: {j.pontuacao}')
+            break
+
+
+if dificuldade == '2':
+    j = Jogador(16) 
+    vida_perdida = 0
+    
+    while True:
+        # cehcagem testa se o input é um um número entre 0 e 1_000_000
+        checagem = (str(x) for x in range(1000000))
+        if random.choice([1, 2, 3]) == 1 or 2 or 3:
+            a, b = random.randint(0, 99), random.randint(0, 99)
+            resposta = input(f'\n{a} + {b} = ')
+            
+            while resposta not in checagem:
+                # checagem = (str(x) for x in range(1000000))
+                resposta = input('Insira um número inteiro')
+
+            if int(resposta) == a + b:
+                print('\ncorreto\n')
+                j.pontuacao += 1
+            else:
+                print('\nerrado\n')
+                vida_perdida += 4
+                j.vida -= 4
+
+        print(f"Vida: ]{'|' * (j.vida)}{int(j.vida / 16 * 100)}%{'-' * vida_perdida}[")
+        print(f'Pontuacao: {j.pontuacao}\n')
+
+        if j.vida == 0 or input('Continuar? s / n\n' ) == 'n':
+            print(f'\nSua pontuação é: {j.pontuacao}')
+            break
+
+
+if dificuldade == '3':
+    j = Jogador(12) 
+    vida_perdida = 0
+    
+    while True:
+        # cehcagem testa se o input é um um número entre 0 e 1_000_000
+        checagem = (str(x) for x in range(1000000))
+        if random.choice([1, 2, 3]) == 1 or 2 or 3:
+            a, b = random.randint(0, 999), random.randint(0, 999)
+            resposta = input(f'\n{a} + {b} = ')
+            
+            while resposta not in checagem:
+                # checagem = (str(x) for x in range(1000000))
+                resposta = input('Insira um número inteiro')
+
+            if int(resposta) == a + b:
+                print('\ncorreto\n')
+                j.pontuacao += 1
+            else:
+                print('\nerrado\n')
+                vida_perdida += 4
+                j.vida -= 4
+
+        print(f"Vida: ]{'|' * (j.vida)}{int(j.vida / 12 * 100)}%{'-' * vida_perdida}[")
+        print(f'Pontuacao: {j.pontuacao}\n')
+
+        if j.vida == 0 or input('Continuar? s / n\n' ) == 'n':
+            print(f'\nSua pontuação é: {j.pontuacao}')
             break
