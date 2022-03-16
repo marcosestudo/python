f152b6d13038c3b46c9144c08fab400d5a34ee2c
Marcos Fernando <estudogit@outlook.com>
2022-03-16 12:49:38 -0300

    sem walrus 1

--- jogo_python_udemy/jogo.py
+++ jogo_python_udemy/jogo.py
@@ -14,15 +14,20 @@ dificuldade = input('''Escolha a dificuldade: digite 1, 2 ou 3
 
 if dificuldade == '1':
     vida_perdida = 0    
-    print(vida := f"vida ]{'|' * (20 - vida_perdida)}{' ' * vida_perdida}[")
+    vida = f"vida ]{'|' * (20 - vida_perdida)}{' ' * vida_perdida}["
+    print(vida)
     print(pontuacao)    
 
     if random.choice([1, 2, 3]) == 1 or 2 or 3:
-        print(pergunta := random.randint(0, 9) + random.randint(0, 9))
-        if resposta:= input(('Resposta: ')) == pergunta:
+        pergunta = random.randint(0, 9) + random.randint(0, 9)
+        print(pergunta)
+        resposta = input('Resposta: ')
+        if resposta == pergunta:
             pontuacao += 1
         else:
             vida_perdida += 4
+        print(f'pontuacao {pontuacao}')
+        print(f'vida_perdida {vida_perdida}')
 
     print(vida)
     print(f'Pontuação: {pontuacao}')
