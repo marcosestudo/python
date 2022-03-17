c03fe9c01046543adcedfeb8aa79d58af8758bc3
Marcos Fernando <estudogit@outlook.com>
2022-03-16 23:18:24 -0300

    finalizado, só falta implementar o rank

--- jogo_python_udemy/jogo.py
+++ jogo_python_udemy/jogo.py
@@ -6,33 +6,34 @@ from jogador import Jogador
 # não esquecer de trocar essa função po ruma lambda
 def vida(vida: int, vida_max: int, vida_perdida: int) -> str:
     """retorna a barra de vida para ser printada"""
-    return f"Vida: ]{'|' * (vida)}{int(vida / vida_max * 100)}%{'-' * vida_perdida}["
+    return f"]{'|' * (vida)}{int(vida / vida_max * 100)}%{'-' * vida_perdida}["
 
 
 def dificuldade(dif: int, jogador: Jogador):
     """Retorna o valores mínimos e máximos e as operações possiveis de acordo com o nível de dificuldade"""
     if dif == '1':
-        jogador.vida = 20
+        jogador.vida = vida_max = 20
         min = -10
         max = 10
         mult = div = False
 
     elif dif == '2':
-        jogador.vida = 16
+        jogador.vida = vida_max = 16
         min = -100
         max = 100
         mult = div = True
 
     elif dif == '3':
-        jogador.vida = 10
+        jogador.vida = vida_max = 10
         min = -100
         max = 100
         mult = div = True
 
-    return min, max, mult, div
+    return min, max, mult, div, vida_max
 
 
 def operacao(min: int, max: int, mult: bool, div: bool, operacao: int = randint(1, 4)) -> int:
+    """retorna uma operação de acordo com a dificuldade"""
     a, b = randint(min, max), randint(min, max) 
 
     if operacao == 1:
@@ -44,11 +45,11 @@ def operacao(min: int, max: int, mult: bool, div: bool, operacao: int = randint(
         print(f'{a} - {abs(b)} = ', end='')
         return a - abs(b)
 
-    elif operacao == 3:
+    elif operacao == 3 and mult:
         print(f'{a} * {b} = ', end='')
         return a * b
 
-    if operacao == 4:
+    if operacao == 4 and div:
         while a < b or b == 0:
             a = b = randint(1, 9)
         print(f'{a * (multiplicador := randint(5, 10))} / {b} = ', end='')
@@ -57,6 +58,7 @@ def operacao(min: int, max: int, mult: bool, div: bool, operacao: int = randint(
 dificuldade_escolhida = None
 
 j = Jogador()
+vida_perdida = 0
 
 while dificuldade_escolhida not in ('1', '2', '3'):
     dificuldade_escolhida = input('''
@@ -71,14 +73,24 @@ dif = dificuldade(dificuldade_escolhida, j)
 while True:
     print()
     gabarito = operacao(dif[0], dif[1], dif[2], dif[3], randint(1, 4))
-    if resposta := int(input()) == gabarito:
+    # no nível fácil não tem operacões de multiplicação e divisão, se cair uma delas rotrnara None e operacao() será reexecutada
+    while gabarito == None:
+        gabarito = operacao(dif[0], dif[1], dif[2], dif[3], randint(1, 4))
+    resposta = input()
+    # checagem = (x for x in range(-1000000, 1000000))
+    while resposta not in (str(x) for x in range(-100000, 100000)):
+        resposta = input('insira um número inteiro\n')
+    
+    if int(resposta) == gabarito:
+        print('Correto')
         j.pontuacao += 1        
     else:
+        print('Errado')
+        vida_perdida += 4
         j.vida -= 4
-
-    print(f'{j.pontuacao}\n{j.vida}')
     
+    print(f'\nPontuacao: {j.pontuacao}\nVida: {vida(j.vida, dif[4], vida_perdida)}\n')
 
-# dif = dificuldade(1)
-# for i in range(10):
-#     print(operacao(dif[0], dif[1], dif[2], dif[3]))
+    if j.vida <= 0 or input('Continuar: s / n\n') == 'n':
+        print(f'\nSua pontuação é: {j.pontuacao}\n')
+        break
