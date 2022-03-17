2e409387432c822a5bdc4876509ff9cfaaebec01
Marcos Fernando <estudogit@outlook.com>
2022-03-17 17:49:38 -0300

    finalizado

--- jogo_python_udemy/jogo.py
+++ jogo_python_udemy/jogo.py
@@ -1,64 +1,15 @@
-from random import randint, random
-from tkinter.tix import Tree
+from random import randint
 from jogador import Jogador
+from rank import atualizar_rank
+from funcoes import vida, dificuldade, operacao
 
-
-# não esquecer de trocar essa função po ruma lambda
-def vida(vida: int, vida_max: int, vida_perdida: int) -> str:
-    """retorna a barra de vida para ser printada"""
-    return f"]{'|' * (vida)}{int(vida / vida_max * 100)}%{'-' * vida_perdida}["
-
-
-def dificuldade(dif: int, jogador: Jogador):
-    """Retorna o valores mínimos e máximos e as operações possiveis de acordo com o nível de dificuldade"""
-    if dif == '1':
-        jogador.vida = vida_max = 20
-        min = -10
-        max = 10
-        mult = div = False
-
-    elif dif == '2':
-        jogador.vida = vida_max = 16
-        min = -100
-        max = 100
-        mult = div = True
-
-    elif dif == '3':
-        jogador.vida = vida_max = 10
-        min = -100
-        max = 100
-        mult = div = True
-
-    return min, max, mult, div, vida_max
-
-
-def operacao(min: int, max: int, mult: bool, div: bool, operacao: int = randint(1, 4)) -> int:
-    """retorna uma operação de acordo com a dificuldade"""
-    a, b = randint(min, max), randint(min, max) 
-
-    if operacao == 1:
-        print(f'{a} + {b} = ', end='')
-        return a + b
-
-    elif operacao == 2:
-        # abs pra não ter dois sinais de menos como "a - -b", por exemplo
-        print(f'{a} - {abs(b)} = ', end='')
-        return a - abs(b)
-
-    elif operacao == 3 and mult:
-        print(f'{a} * {b} = ', end='')
-        return a * b
-
-    if operacao == 4 and div:
-        while a < b or b == 0:
-            a = b = randint(1, 9)
-        print(f'{a * (multiplicador := randint(5, 10))} / {b} = ', end='')
-        return a * multiplicador // b
 
 dificuldade_escolhida = None
 
+
 j = Jogador()
 vida_perdida = 0
+
 
 while dificuldade_escolhida not in ('1', '2', '3'):
     dificuldade_escolhida = input('''
@@ -72,11 +23,14 @@ dif = dificuldade(dificuldade_escolhida, j)
 
 while True:
     print()
+
     gabarito = operacao(dif[0], dif[1], dif[2], dif[3], randint(1, 4))
+
     # no nível fácil não tem operacões de multiplicação e divisão, se cair uma delas rotrnara None e operacao() será reexecutada
     while gabarito == None:
         gabarito = operacao(dif[0], dif[1], dif[2], dif[3], randint(1, 4))
     resposta = input()
+
     # checagem = (x for x in range(-1000000, 1000000))
     while resposta not in (str(x) for x in range(-100000, 100000)):
         resposta = input('insira um número inteiro\n')
@@ -88,9 +42,13 @@ while True:
         print('Errado')
         vida_perdida += 4
         j.vida -= 4
-    
+
     print(f'\nPontuacao: {j.pontuacao}\nVida: {vida(j.vida, dif[4], vida_perdida)}\n')
+
 
     if j.vida <= 0 or input('Continuar: s / n\n') == 'n':
         print(f'\nSua pontuação é: {j.pontuacao}\n')
         break
+
+
+atualizar_rank(j.pontuacao, input('Insira o teu nome: '))
--- jogo_python_udemy/rank.py
+++ jogo_python_udemy/rank.py
@@ -1,54 +1,17 @@
-# linhas = [2, 1, 5, 3, 4, 8, 6, 9, 10, 7]
-
-linhas_ordenadas = []
-
-with open('rank.txt', 'r', encoding='utf8') as arquivo:
-    # lendo a primeir alinha sem atribuí-la para pular o cabeçãlho
-    arquivo.readline()
-    rank = arquivo.read().split('\n')
-    
-# print(rank)
-
-ordenar = []
-
-for linha in rank:
-    # print(linha.split(',')[0])
-    ordenar.append(linha.split(',')[0])
-
-# limpando espaços vazios, se tiver
-while '' in ordenar:
-    ordenar.remove('')
-while '' in rank:
-    rank.remove('')
-
-print(ordenar)
-
-ordenar = [int(num) for num in ordenar]
-ordenar.sort()
-print(ordenar)
-
-print(rank[0])
-# print(len(rank))
-# for num in rank:
-#     linhas_ordenadas.append(int(num))
-
-# linhas_ordenadas.sort()
-# print(linhas_ordenadas)
-
-# with open('rank_ordenado.txt', 'w', encoding='utf8') as arquivo:
-#     for linha in linhas_ordenadas:
-#         arquivo.write(str(linha) + '\n')
-
-"""
 from csv import DictReader
 from csv import DictWriter
 
-def atualizar_rank(pontuacao, nome):
+
+def atualizar_rank(pontuacao: int, nome: str, adicionado: int = 0) -> None:
+    """Recebe a pontuação e o nome do jogador, adiciona no rank e o coloca em ordem.
+    O parâmetro "adicionado" diz se o elemento já foi adicionado à lista parar evitar 
+    repetição no caso de empate.    
+    """
     lista = []
 
-    # with open('rank.csv', 'a', encoding='utf_8', newline='') as arquivo:
-    #     escritor_csv = DictWriter(arquivo, fieldnames=['Pontuação', 'Nome'])
-    #     escritor_csv.writerow(pontuacao, nome)
+    with open('rank.csv', 'a', encoding='utf_8', newline='') as arquivo:
+        escritor_csv = DictWriter(arquivo, fieldnames=['Pontuação', 'Nome', 'Adicionado'])
+        escritor_csv.writerow({'Pontuação' : pontuacao, 'Nome' : nome, 'Adicionado' : adicionado})
 
     with open('rank.csv', encoding='utf_8') as arquivo:
         rank = list(DictReader(arquivo, delimiter=','))
@@ -60,26 +23,30 @@ def atualizar_rank(pontuacao, nome):
 
     rank_ordenado = []
     for i in range(len(lista)):
-        for i in range(len(rank)):
-            if int(rank[i]['Pontuação']) == lista[i]:
-                rank_ordenado.append(rank[i])
+        for j in range(len(rank)):
+            if int(rank[j]['Pontuação']) == lista[i] and not int(rank[j]['Adicionado']):
+                rank_ordenado.append(rank[j])
+                rank[j]['Adicionado'] = 1
 
-    with open('rank_ordenado.csv', 'w', encoding='utf_8', newline='') as arquivo:
-        escritor_csv = DictWriter(arquivo, fieldnames=['Pontuação', 'Nome'])
+    # zerando os "adicionados" para gravar no documento com todos zerados para a próxima atualização do rank
+    for rank in rank_ordenado:
+        rank['Adicionado'] = 0
+
+
+    with open('rank.csv', 'w', encoding='utf_8', newline='') as arquivo:
+        escritor_csv = DictWriter(arquivo, fieldnames=['Pontuação', 'Nome', 'Adicionado'])
         escritor_csv.writeheader()
         for i in range(len(rank_ordenado) - 1, -1, -1):
             escritor_csv.writerow(rank_ordenado[i])
 
-    print(pontuacao, nome)
+    with open('rank.csv', encoding='utf_8') as arquivo:
+        rank = list(DictReader(arquivo, delimiter=','))
+        print('\n=||| RANKING |||=')
 
-atualizar_rank('asd', 100)
-
-""" 
-"""
-Pontuação,Nome
-5,e
-2,b
-4,d
-1,a
-3,c
-"""
+        for linha in range(5):
+            if int(rank[linha]['Pontuação']) < 10:
+                print(f"00{rank[linha]['Pontuação']}   {rank[linha]['Nome']}")
+            elif int(rank[linha]['Pontuação']) < 100:
+                print(f"0{rank[linha]['Pontuação']}   {rank[linha]['Nome']}")
+            else:
+                print(f"0{rank[linha]['Pontuação']}   {rank[linha]['Nome']}")
